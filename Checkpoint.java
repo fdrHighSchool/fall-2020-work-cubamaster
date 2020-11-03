4158b01660427fe07f6f4eb0c73b815a57522f4b
Murat Khidoyatov <muratkhidoyatov@MacBook-Air-Murat.local>
2020-11-03 09:15:07 -0500

    First commit

--- Checkpoint.java
+++ Checkpoint.java
@@ -7,13 +7,21 @@ Checkpoint {
         System.out.println("Write your problem");
         String problem = scanner.nextLine();
         produceAnswer(problem);
+        String second = produceAnswer(problem);
+        System.out.println(second);
     }
-    public static double produceAnswer(String problem) {
+    /*
+     *
+     *
+     *
+     *
+     *
+     */
+
+    public static String  produceAnswer(String problem) {
         int plus = problem.indexOf("+");
         String x1 = problem.substring(0, plus-1);
-        System.out.printf("The first fraction is %s\n", x1);
         String x2 = problem.substring(plus+2, problem.length());
-        System.out.printf("The second fraction is %s\n", x2);
-        return 0;
+        return x2;
     }
 }
