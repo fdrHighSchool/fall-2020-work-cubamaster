a4bf8506355a4e898bd19ce3d0324f0f44408aa4
Murat Khidoyatov <muratkhidoyatov@MacBook-Air-Murat.local>
2020-11-09 17:58:00 -0500

    Added sum function

--- 08_fraction_calculator/chekpoint#3.java
+++ 08_fraction_calculator/chekpoint#3.java
@@ -1,169 +1,112 @@
-{\rtf1\ansi\ansicpg1251\cocoartf2513
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fmodern\fcharset0 Courier;\f1\fmodern\fcharset0 Courier-Oblique;}
-{\colortbl;\red255\green255\blue255;\red191\green100\blue38;\red32\green32\blue32;\red153\green168\blue186;
-\red254\green187\blue91;\red133\green96\blue154;\red109\green109\blue109;\red88\green118\blue71;\red86\green132\blue173;
-}
-{\*\expandedcolortbl;;\csgenericrgb\c74902\c39216\c14902;\csgenericrgb\c12549\c12549\c12549;\csgenericrgb\c60000\c65882\c72941;
-\csgenericrgb\c99608\c73333\c35686;\csgenericrgb\c52157\c37647\c60392;\csgenericrgb\c42745\c42745\c42745;\csgenericrgb\c34510\c46275\c27843;\csgenericrgb\c33725\c51765\c67843;
-}
-\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\pard\tx560\tx1120\tx1680\tx2240\tx2800\tx3360\tx3920\tx4480\tx5040\tx5600\tx6160\tx6720\pardirnatural\partightenfactor0
+import java.util.*;
+public class
+Checkpoint {
 
-\f0\fs28 \cf2 \cb3 import \cf4 java.util.*\cf2 ;\
-public class\
-\cf4 Checkpoint \{\
-\
-    \cf2 public static void \cf5 main\cf4 (String[] args) \{\
-        Scanner scanner = \cf2 new \cf4 Scanner(System.
-\f1\i \cf6 in
-\f0\i0 \cf4 )\cf2 ; \cf7 // Input scanner method\
-        \cf4 System.
-\f1\i \cf6 out
-\f0\i0 \cf4 .println(\cf8 "Write your problem. If you want to stop, write quit "\cf4 )\cf2 ;\
-        \cf4 String problem = \cf8 " "\cf2 ;\
-        boolean \cf4 check = \cf2 true;\
-        while \cf4 (check) \{\
-            problem = scanner.nextLine()\cf2 ;\
-            if \cf4 (problem.contains(\cf8 "+"\cf4 ) || problem.contains(\cf8 "-"\cf4 ) || problem.contains(\cf8 "*"\cf4 ) || problem.contains(\cf8 "/"\cf4 )) \{\
-                System.
-\f1\i \cf6 out
-\f0\i0 \cf4 .println(\cf8 "OK"\cf4 )\cf2 ;\
-                \cf4 check = \cf2 false;\
-            \cf4 \}\
-            \cf2 else if \cf4 (problem.equals(\cf8 "quit"\cf4 )) \{\
-                \cf2 return;\
-            \cf4 \}\
-            \cf2 else \cf4 \{\
-                System.
-\f1\i \cf6 out
-\f0\i0 \cf4 .println(\cf8 "Your input incorrect, try again"\cf4 )\cf2 ;\
-            \cf4 \}\
-        \}\
-        String second = 
-\f1\i second_fraction
-\f0\i0 (problem)\cf2 ; \cf7 // take second fraction as a string\
-        \cf4 String first = 
-\f1\i first_fraction
-\f0\i0 (problem)\cf2 ;\
-        \cf4 System.
-\f1\i \cf6 out
-\f0\i0 \cf4 .println(first)\cf2 ;\
-        \cf4 System.
-\f1\i \cf6 out
-\f0\i0 \cf4 .println(second)\cf2 ;\
-        
-\f1\i \cf4 produceAnswer
-\f0\i0 (problem)\cf2 ;\
-    \cf4 \}\cf7 // close main\
-    /*\
-    * main - main function\
-    * produceAnswer - Function where I determine first and second fraction\
-    * x1- first one\
-    * x2 - second\
-     */\
-    /*\
-    produceAnswer is function which take second fraction as String\
-     */\
-    \cf2 public static \cf4 String  \cf5 second_fraction\cf4 (String problem) \{\
-        \cf2 int \cf4 space = problem.indexOf(\cf8 " "\cf4 )\cf2 ; \cf7 // only what I use is "+" symbol\
-        \cf4 String x2 = problem.substring(space+\cf9 3\cf2 , \cf4 problem.length())\cf2 ; \cf7 // second fraction\
-        \cf2 return \cf4 x2\cf2 ;\
-    \cf4 \} \cf7 // close produceAnswer\
-    \cf2 public static \cf4 String \cf5 first_fraction\cf4 (String problem) \{\
-        \cf2 int \cf4 space = problem.indexOf(\cf8 " "\cf4 )\cf2 ;\
-        \cf4 String x1 = problem.substring(\cf9 0\cf2 , \cf4 space)\cf2 ;\
-        return \cf4 x1\cf2 ;\
-    \cf4 \}\
-    \cf2 public static \cf4 String \cf5 produceAnswer\cf4 (String problem)\{\
-        String first_fraction = 
-\f1\i first_fraction
-\f0\i0 (problem)\cf2 ;\
-        \cf4 String second_fraction = 
-\f1\i second_fraction
-\f0\i0 (problem)\cf2 ;\
-        int \cf4 first_whole = 
-\f1\i whole
-\f0\i0 (first_fraction)\cf2 ;\
-        int \cf4 first_numerator = 
-\f1\i numerator
-\f0\i0 (first_fraction)\cf2 ;\
-        int \cf4 first_denominator = 
-\f1\i denominator
-\f0\i0 (first_fraction)\cf2 ;\
-        int \cf4 second_whole = 
-\f1\i whole
-\f0\i0 (second_fraction)\cf2 ;\
-        int \cf4 second_numerator = 
-\f1\i numerator
-\f0\i0 (second_fraction)\cf2 ;\
-        int \cf4 second_denominator = 
-\f1\i denominator
-\f0\i0 (second_fraction)\cf2 ;\
-        int \cf4 space = problem.indexOf(\cf8 " "\cf4 )\cf2 ;\
-        \cf4 String symbol = problem.substring(space+\cf9 1\cf2 , \cf4 space+\cf9 2\cf4 )\cf2 ;\
-        \cf4 System.
-\f1\i \cf6 out
-\f0\i0 \cf4 .println(symbol)\cf2 ;\
-        return \cf8 "Hello"\cf2 ;\
-    \cf4 \}\
-    \cf7 /*\
-    whole/numerator/denominator is functions from checkpoint#2\
-     */\
-    \cf2 public static int \cf5 whole\cf4 (String fraction) \{\
-        \cf2 int \cf4 underscore = fraction.indexOf(\cf8 "_"\cf4 )\cf2 ;\
-        int \cf4 division = fraction.indexOf(\cf8 "/"\cf4 )\cf2 ;\
-        \cf4 String x1\cf2 ;\
-        if \cf4 (underscore == -\cf9 1\cf4 ) \{\
-            x1 = fraction.substring(\cf9 0\cf2 , \cf4 fraction.length())\cf2 ;\
-            if \cf4 (division != -\cf9 1\cf4 ) \{\
-                x1 = \cf8 "0"\cf2 ;\
-                return \cf4 Integer.
-\f1\i parseInt
-\f0\i0 (x1)\cf2 ;\
-            \cf4 \}\
-            \cf2 return \cf4 Integer.
-\f1\i parseInt
-\f0\i0 (x1)\cf2 ;\
-        \cf4 \}\
-        x1 = fraction.substring(\cf9 0\cf2 , \cf4 underscore)\cf2 ;\
-        return \cf4 Integer.
-\f1\i parseInt
-\f0\i0 (x1)\cf2 ;\
-    \cf4 \}\cf7 // close whole\
-    \cf2 public static int \cf5 numerator\cf4 (String fraction) \{\
-        \cf2 int \cf4 underscore = fraction.indexOf(\cf8 "_"\cf4 )\cf2 ;\
-        int \cf4 division = fraction.indexOf(\cf8 "/"\cf4 )\cf2 ;\
-        \cf4 String x2\cf2 ;\
-        if \cf4 (underscore == -\cf9 1 \cf4 && division == -\cf9 1 \cf4 ) \{\
-            x2 = \cf8 "0"\cf2 ;\
-            return \cf4 Integer.
-\f1\i parseInt
-\f0\i0 (x2)\cf2 ;\
-        \cf4 \}\
-        \cf2 if \cf4 (underscore == -\cf9 1\cf4 ) \{\
-            x2 = fraction.substring(\cf9 0\cf2 , \cf4 division)\cf2 ;\
-            return \cf4 Integer.
-\f1\i parseInt
-\f0\i0 (x2)\cf2 ;\
-        \cf4 \}\
-        x2 = fraction.substring(underscore + \cf9 1\cf2 , \cf4 division)\cf2 ;\
-        return \cf4 Integer.
-\f1\i parseInt
-\f0\i0 (x2)\cf2 ;\
-    \cf4 \}\cf7 // close numeration\
-    \cf2 public static int \cf5 denominator\cf4 (String fraction) \{\
-        \cf2 int \cf4 division = fraction.indexOf(\cf8 "/"\cf4 )\cf2 ;\
-        \cf4 String x3\cf2 ;\
-        if \cf4 (division == -\cf9 1\cf4 ) \{\
-            x3 = \cf8 "1"\cf2 ;\
-            return \cf4 Integer.
-\f1\i parseInt
-\f0\i0 (x3)\cf2 ;\
-        \cf4 \}\
-        x3 = fraction.substring(division+\cf9 1\cf2 , \cf4 fraction.length())\cf2 ;\
-        return \cf4 Integer.
-\f1\i parseInt
-\f0\i0 (x3)\cf2 ;\
-    \cf4 \}\cf7 // close denominator\
-\cf4 \}\cf7 // close class\
-}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); // Input scanner method
+        System.out.println("Write your problem. If you want to stop, write quit ");
+        String problem = " ";
+        boolean check = true;
+        while (check) {
+            problem = scanner.nextLine();
+            if (problem.contains("+") || problem.contains("-") || problem.contains("*") || problem.contains("/")) {
+                System.out.println("OK");
+                check = false;
+            }
+            else if (problem.equals("quit")) {
+                return;
+            }
+            else {
+                System.out.println("Your input incorrect, try again");
+            }
+        }
+        String second = second_fraction(problem); // take second fraction as a string
+        String first = first_fraction(problem);
+        System.out.println(first);
+        System.out.println(second);
+        produceAnswer(problem);
+    }// close main
+    /*
+    * main - main function
+    * produceAnswer - Function where I determine first and second fraction
+    * x1- first one
+    * x2 - second
+     */
+    /*
+    produceAnswer is function which take second fraction as String
+     */
+    public static String  second_fraction(String problem) {
+        int space = problem.indexOf(" "); // only what I use is "+" symbol
+        String x2 = problem.substring(space+3, problem.length()); // second fraction
+        return x2;
+    } // close produceAnswer
+    public static String first_fraction(String problem) {
+        int space = problem.indexOf(" ");
+        String x1 = problem.substring(0, space);
+        return x1;
+    }
+    public static String produceAnswer(String problem){
+        String first_fraction = first_fraction(problem);
+        String second_fraction = second_fraction(problem);
+        int first_whole = whole(first_fraction);
+        int first_numerator = numerator(first_fraction);
+        int first_denominator = denominator(first_fraction);
+        int first_numerator_2 = first_whole * first_denominator + first_numerator;
+        int second_whole = whole(second_fraction);
+        int second_numerator = numerator(second_fraction);
+        int second_denominator = denominator(second_fraction);
+        int second_numerator_2 = second_whole * second_denominator + second_numerator;
+        int space = problem.indexOf(" ");
+        String symbol = problem.substring(space+1, space+2);
+        System.out.println(symbol);
+        if (symbol.equals("+")) {
+            int third_denominator = first_denominator * second_denominator;
+            int third_numerator = (first_numerator_2 * second_denominator) + (second_numerator_2 * first_denominator);
+            System.out.println(third_numerator + "/" + third_denominator);
+        } 
+        return "Hello";
+    }
+    /*
+    whole/numerator/denominator is functions from checkpoint#2
+     */
+    public static int whole(String fraction) {
+        int underscore = fraction.indexOf("_");
+        int division = fraction.indexOf("/");
+        String x1;
+        if (underscore == -1) {
+            x1 = fraction.substring(0, fraction.length());
+            if (division != -1) {
+                x1 = "0";
+                return Integer.parseInt(x1);
+            }
+            return Integer.parseInt(x1);
+        }
+        x1 = fraction.substring(0, underscore);
+        return Integer.parseInt(x1);
+    }// close whole
+    public static int numerator(String fraction) {
+        int underscore = fraction.indexOf("_");
+        int division = fraction.indexOf("/");
+        String x2;
+        if (underscore == -1 && division == -1 ) {
+            x2 = "0";
+            return Integer.parseInt(x2);
+        }
+        if (underscore == -1) {
+            x2 = fraction.substring(0, division);
+            return Integer.parseInt(x2);
+        }
+        x2 = fraction.substring(underscore + 1, division);
+        return Integer.parseInt(x2);
+    }// close numeration
+    public static int denominator(String fraction) {
+        int division = fraction.indexOf("/");
+        String x3;
+        if (division == -1) {
+            x3 = "1";
+            return Integer.parseInt(x3);
+        }
+        x3 = fraction.substring(division+1, fraction.length());
+        return Integer.parseInt(x3);
+    }// close denominator
+}// close class
