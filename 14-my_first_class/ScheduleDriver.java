cfeb7e08088d1712b7e2a5c4424fa39b2b5f0c5c
cubamaster <muratkhid1129@gmail.com>
2021-01-19 19:25:11 -0500

    now you see how classes works

--- 14-my_first_class/ScheduleDriver.java
+++ 14-my_first_class/ScheduleDriver.java
@@ -4,12 +4,21 @@ public class ScheduleDriver {
     public static void main(String[] args) {
         Schedule schedule1 = new Schedule();
         Scanner scanner = new Scanner(System.in);
-        for (int i = 0; i < schedule1.getSubjects().length; i++) {
-            System.out.println("Please write your period " + (i+1) + " subject");
-            String subject = scanner.nextLine();
-            schedule1.addSubject(subject);
-        }
+
+        schedule1.addSubject("AP CSA");
+        schedule1.addSubject("English");
+        schedule1.addSubject("History");
+        schedule1.addSubject("Wellness");
+        schedule1.addSubject("Art");
+        schedule1.addSubject("Chemistry");
+        schedule1.addSubject("Pre-calculus");
         schedule1.getSubjectsSchedule();
+        System.out.println("");
+        schedule1.getSubjectsSchedule();
+        System.out.println("");
+        schedule1.changeSubject("Physics",6);
+        schedule1.getSubjectsSchedule();
+        System.out.println("");
 
 
         for (int i = 0; i < schedule1.getBell().length; i++) {
